a239f2624e55e5a4b5c4ae6f4df557262f278013
fed1k <allamurotovcoder@mail.ru>
2022-08-22 23:29:22 +0500

    Add form and form components with little styling

--- src/components/AddReservation.jsx
+++ src/components/AddReservation.jsx
@@ -1,9 +1,47 @@
+import { useState } from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { useSelector } from 'react-redux';
+import TextField from '@mui/material/TextField';
+import SendIcon from '@mui/icons-material/Send';
+import Button from '@mui/material/Button';
+
 const AddReservation = () => {
-    return (
-        <div className="add-reservation-page">
-            <h1>Add new reservation</h1>
+  const [age, setAge] = useState('');
+  const apartments = useSelector((state) => state);
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  const logger = () => {
+    console.log('uhhhhhu');
+  };
+
+  return (
+    <form action="" className="reservation-form">
+      <FormControl sx={{ m: 1, minWidth: 120 }} size="small" className='ui-form-for-reservation'>
+        <InputLabel id="demo-select-small">Name</InputLabel>
+        <Select labelId="demo-select-small" id="demo-select-small" value={age} label="Name" onChange={handleChange} required>
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          {apartments ? apartments.map((apartment) => (
+            <MenuItem key={apartment.id} value={apartment.id}>{apartment.name}</MenuItem>
+          )) : <span> </span>}
+        </Select>
+        <TextField id="demo-helper-text-misaligned-no-helper" label="Amount" type="number" required />
+        <div className="datetime-div">
+          <input type="date" required />
+          <span>to</span>
+          <input type="date" required />
         </div>
-    );
-}
- 
+        <Button type="submit" className="add-reservation-btn" style={{backgroundColor: '#97C00EFF'}} variant="contained" endIcon={<SendIcon />}>Send</Button>
+      </FormControl>
+    </form>
+  );
+};
+
 export default AddReservation;
