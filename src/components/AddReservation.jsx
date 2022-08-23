1582c67b7f6220ef4f5e0775076a7e2181c73e5a
fed1k <allamurotovcoder@mail.ru>
2022-08-24 02:10:46 +0500

    Work on my reservations page with styling and styling for add reservation

--- src/components/AddReservation.jsx
+++ src/components/AddReservation.jsx
@@ -1,30 +1,26 @@
-import { useState } from "react";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import { useSelector } from "react-redux";
-import TextField from "@mui/material/TextField";
-import SendIcon from "@mui/icons-material/Send";
-import Button from "@mui/material/Button";
-import { useNavigate } from "react-router-dom";
-import Swal from "sweetalert2";
-import { auth } from "../redux/reducer";
+import { useState } from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { useSelector } from 'react-redux';
+import SendIcon from '@mui/icons-material/Send';
+import Button from '@mui/material/Button';
+import Swal from 'sweetalert2';
 
 const AddReservation = () => {
-  const [age, setAge] = useState("");
+  const [age, setAge] = useState('');
   const [amount, setAmount] = useState(0);
   const [sDate, setSDate] = useState();
   const [eDate, setEDate] = useState();
   const [picked, setPicked] = useState();
 
   const apartments = useSelector((state) => state);
-  const navigate = useNavigate();
 
   const handleChange = (event) => {
     setAge(event.target.value);
     const pickedApartment = apartments.filter(
-      (apartment) => apartment.id === event.target.value
+      (apartment) => apartment.id === event.target.value,
     );
     setPicked(pickedApartment);
   };
@@ -34,53 +30,28 @@ const AddReservation = () => {
   };
   const handleEndDate = (e) => {
     setEDate(e.target.value);
+    const startDate = new Date(sDate);
+    const endDate = new Date(e.target.value);
+    const difference = endDate.getTime() - startDate.getTime();
+    const TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
+    setAmount(TotalDays * Math.round(picked[0].price / 30));
   };
 
   const submit = (e) => {
     e.preventDefault();
     if (eDate > sDate) {
       Swal.fire({
-        position: "center",
-        icon: "success",
-        title: "Your work has been saved",
+        position: 'center',
+        icon: 'success',
+        title: 'Your work has been saved',
         showConfirmButton: false,
         timer: 1300,
       });
-      const startDate = new Date(sDate);
-      const endDate = new Date(eDate);
-      const difference = endDate.getTime() - startDate.getTime();
-      const TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
-      setAmount(TotalDays * picked[0].price);
-      // const difference = sDate.getTime() - eDate.getTime();
-      // console.log(difference);
-      // console.log(age);
-      // console.log((((parseInt(eDate) - parseInt(sDate)).to_f / 365) * 12).round);
-      // console.log(parseInt(eDate - sDate));
-      // console.log(typeof eDate);
-      //   const data = {
-      //     startDate: sDate,
-      //     endDate: eDate,
-      //     apartment_id: age,
-      //     amount,
-      //     user_id: JSON.parse(localStorage.getItem('current_user')).id,
-      //   };
-      //   fetch('https://zuku-apartments-api.herokuapp.com/api/v1/reservations', {
-      //     method: 'POST',
-      //     body: JSON.stringify(data),
-      //     headers: {
-      //       Authorization: auth,
-      //       Accept: 'application/json',
-      //       'Content-Type': 'application/json',
-      //     },
-      //   });
-      //   setTimeout(() => {
-      //     navigate('/my_reservations');
-      //   }, 1500);
     } else {
       Swal.fire({
-        position: "center",
-        icon: "error",
-        title: "End Date should be greater than start date",
+        position: 'center',
+        icon: 'error',
+        title: 'End Date should be greater than start date',
         showConfirmButton: false,
         timer: 2000,
       });
@@ -94,7 +65,7 @@ const AddReservation = () => {
         size="small"
         className="ui-form-for-reservation"
       >
-        <InputLabel id="demo-select-small">Name of Apartment</InputLabel>
+        <InputLabel id="demo-select-small" style={{ color: 'white' }}>Name of Apartment</InputLabel>
         <Select
           labelId="demo-select-small"
           id="demo-select-small"
@@ -125,21 +96,18 @@ const AddReservation = () => {
           <span>to</span>
           <input type="date" onChange={handleEndDate} required />
         </div>
-        <TextField
-          id="demo-helper-text-misaligned-no-helper"
-          label="Amount $"
-          type="number"
-          disabled
-          value={`${amount}`}
-        />
+        <fieldset>
+          <legend>Amount $</legend>
+          <input type="number" id="mothman" name="monster" disabled value={amount} />
+        </fieldset>
         <Button
           type="submit"
           className="add-reservation-btn"
-          style={{ backgroundColor: "#97C00EFF" }}
+          style={{ backgroundColor: '#97C00EFF' }}
           variant="contained"
           endIcon={<SendIcon />}
         >
-          Send
+          Book
         </Button>
       </FormControl>
     </form>
--- src/components/MyReservations.jsx
+++ src/components/MyReservations.jsx
@@ -1,10 +1,48 @@
-import { useEffect } from "react";
-import { auth } from "../redux/reducer";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux"
+import { GoLocation } from "react-icons/go"
 
 const MyReservations = () => {
+  const [userReservations, setUserReservations] = useState();
+  const data = useSelector((state) => state)
+  useEffect(() => {
+    const fuku = async () => {
+      const data = await fetch(
+        "https://zuku-apartments-api.herokuapp.com/api/v1/reservations",
+        {
+          method: "GET",
+          headers: {
+            Authorization: JSON.parse(localStorage.getItem("token")),
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const response = await data.json();
+      const actual = response.filter(
+        (i) => i.user_id === JSON.parse(localStorage.getItem("current_user")).id
+      );
+      setUserReservations(actual);
+    };
+    fuku();
+  }, []);
+
   return (
     <div className="my-reservations-page">
-      <h1>RESERVATIONS</h1>
+      <h1>My reservations</h1>
+      {userReservations ? (
+        userReservations.map((userReservation) => (
+          <div key={userReservation.id}>
+            <p>Name: {data.filter((i)=> i.id === userReservation.apartment_id)[0].name}</p>
+            <p><GoLocation className="location" />{data.filter((i)=> i.id === userReservation.apartment_id)[0].city}</p>
+            <p>Start date: {userReservation.startDate}</p>
+            <p>End date: {userReservation.endDate}</p>
+            <p>Amount: {userReservation.amount}</p>
+          </div>
+        ))
+      ) : (
+        <h1>Loading...</h1>
+      )}
     </div>
   );
 };
