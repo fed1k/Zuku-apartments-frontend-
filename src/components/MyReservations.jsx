b747a90d109add6557fbf8203f0c24d2232b6de7
fed1k <allamurotovcoder@mail.ru>
2022-08-24 04:20:09 +0500

    Work on mobile responsiveness for add and my reservations pages and post reservation

--- src/components/MyReservations.jsx
+++ src/components/MyReservations.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux"
 import { GoLocation } from "react-icons/go"
+import { BiLeftArrow } from "react-icons/bi"
+import { useNavigate } from "react-router-dom"
 
 const MyReservations = () => {
   const [userReservations, setUserReservations] = useState();
   const data = useSelector((state) => state)
+  const navigate = useNavigate()
   useEffect(() => {
     const fuku = async () => {
       const data = await fetch(
@@ -29,6 +32,9 @@ const MyReservations = () => {
 
   return (
     <div className="my-reservations-page">
+      <button type="button" onClick={() => navigate('/')} className="left-button left-right-buttons add-apartment-page-back-btn white-back-btn my-reservations-back-btn">
+        <BiLeftArrow className="direction-icons green-direction-icon" />
+      </button>
       <h1>My reservations</h1>
       {userReservations ? (
         userReservations.map((userReservation) => (
@@ -37,7 +43,7 @@ const MyReservations = () => {
             <p><GoLocation className="location" />{data.filter((i)=> i.id === userReservation.apartment_id)[0].city}</p>
             <p>Start date: {userReservation.startDate}</p>
             <p>End date: {userReservation.endDate}</p>
-            <p>Amount: {userReservation.amount}</p>
+            <p>Amount: ${userReservation.amount}</p>
           </div>
         ))
       ) : (
