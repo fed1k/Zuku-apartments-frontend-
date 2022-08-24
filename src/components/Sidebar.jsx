29f9dfa71296a58f2f8c40d98ab87cc223c68416
fed1k <allamurotovcoder@mail.ru>
2022-08-24 06:20:04 +0500

    Import logo image

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,28 +1,30 @@
-import { NavLink } from 'react-router-dom';
-import { BsTwitter, BsVimeo } from 'react-icons/bs';
-import { FaFacebookF, FaPinterestP } from 'react-icons/fa';
-import { TiSocialGooglePlus } from 'react-icons/ti';
-import PropTypes from 'prop-types';
-import { FaUserCircle } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import { BsTwitter, BsVimeo } from "react-icons/bs";
+import { FaFacebookF, FaPinterestP, FaUserCircle } from "react-icons/fa";
+import { TiSocialGooglePlus } from "react-icons/ti";
+import PropTypes from "prop-types";
+import logo from "../../181869.svg";
 
 const Sidebar = ({ setIn }) => {
   const closeSideBar = () => {
-    const something = window.document.querySelector('.sidebar');
-    something.classList.remove('sidebar-active');
+    const something = window.document.querySelector(".sidebar");
+    something.classList.remove("sidebar-active");
   };
 
   const signOut = () => {
-    setIn(false)
-    localStorage.removeItem('current_user')
-    localStorage.removeItem('token')
-  }
+    setIn(false);
+    localStorage.removeItem("current_user");
+    localStorage.removeItem("token");
+  };
 
   const currentUser = JSON.parse(localStorage.getItem("current_user")).name;
 
   return (
     <div className="sidebar">
-      <span className="close-sidebar" onClick={closeSideBar}>X</span>
-      <img className="logo" src="../../181869.svg" alt="logo" />
+      <span className="close-sidebar" onClick={closeSideBar}>
+        X
+      </span>
+      <img className="logo" src={logo} alt="logo" />
       <div>
         <span className="current-user-name">
           <FaUserCircle />
@@ -30,12 +32,36 @@ const Sidebar = ({ setIn }) => {
         </span>
       </div>
       <div id="links-div">
-        <NavLink onClick={closeSideBar} className="nav-link" to="/">APARTMENTS</NavLink>
-        <NavLink onClick={closeSideBar} className="nav-link" to="/add_house">ADD APARTMENT</NavLink>
-        <NavLink onClick={closeSideBar} className="nav-link" to="/delete_house">DELETE APARTMENT</NavLink>
-        <NavLink onClick={closeSideBar} className="nav-link" to="/my_reservations">MY RESERVATIONS</NavLink>
-        <NavLink onClick={closeSideBar} className="nav-link" to="/add_reservation">ADD RESERVATION</NavLink>
-        <button type="button" className="nav-link sign-out-btn" onClick={signOut}>SIGN OUT</button>
+        <NavLink onClick={closeSideBar} className="nav-link" to="/">
+          APARTMENTS
+        </NavLink>
+        <NavLink onClick={closeSideBar} className="nav-link" to="/add_house">
+          ADD APARTMENT
+        </NavLink>
+        <NavLink onClick={closeSideBar} className="nav-link" to="/delete_house">
+          DELETE APARTMENT
+        </NavLink>
+        <NavLink
+          onClick={closeSideBar}
+          className="nav-link"
+          to="/my_reservations"
+        >
+          MY RESERVATIONS
+        </NavLink>
+        <NavLink
+          onClick={closeSideBar}
+          className="nav-link"
+          to="/add_reservation"
+        >
+          ADD RESERVATION
+        </NavLink>
+        <button
+          type="button"
+          className="nav-link sign-out-btn"
+          onClick={signOut}
+        >
+          SIGN OUT
+        </button>
       </div>
       <div className="social-contact-container">
         <BsTwitter className="social-icon" />
